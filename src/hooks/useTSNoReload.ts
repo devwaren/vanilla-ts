151d936a8d77d25b90a183cda6b03add500627da
Waren Gador <waren.gador15@gmail.com>
2025-09-20 23:52:22 +0800

    update

--- src/hooks/useTSNoReload.ts
+++ src/hooks/useTSNoReload.ts
@@ -2,8 +2,10 @@ import { useAnchor } from './useTSAnchor';
 import { useTSSelect } from './useTSSelect';
 
 const useTSNoReload = (DOM: HTMLElement) => {
+    if (!DOM) return;
     const anchors = useTSSelect("a", DOM) as NodeListOf<HTMLAnchorElement> | null
     useAnchor(anchors);
+    document.addEventListener("click", e => e.preventDefault());
 };
 
 export { useTSNoReload };
